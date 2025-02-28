37fd9cdfddb110d859e59bf5233c3dfba18c6a9a
ezequiascipriano <65565936+ezequiascipriano@users.noreply.github.com>
2025-02-27 23:02:34 -0300

    base: '/supermercado/'

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/supermercado/', // Ajuste conforme o nome do repositório 
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
